06e692e00c82a4e0394ec68addd34e24ef52e8db
ajay4985622 <ajay@vitto.money>
2025-07-13 12:38:55 +0530

    fixed tracks return value

--- routes/tracks.js
+++ routes/tracks.js
@@ -4,10 +4,14 @@ const { getTopTracks } = require('../services/tracks');
 const router = express.Router();
 
 
-router.get('/top-tracks', async (req, res) => {
+router.get('/', async (req, res) => {
+    const { count='10' } = req.query;
     try {
-        const data = await getTopTracks(10);
-        res.status(200).json({data: data.items});
+        const data = await getTopTracks(parseInt(count, 10));
+        if (!data || data.length === 0) {
+            return res.status(404).json({ error: 'No top tracks found' });
+        }
+        res.status(200).json({data});
     } catch (error) {
         console.error('Error fetching top tracks:', error);
         res.status(500).json({ error: 'Internal Server Error' });
--- services/tracks.js
+++ services/tracks.js
@@ -4,7 +4,25 @@ const getTopTracks = async (count=10) => {
     try {
         const res = await fetchWebApi(`me/top/tracks?limit=${count}`, 'GET');
         // console.log(res);
-        return res;
+        if (res &&  res.items) {
+            return res.items.map((item) => {
+                return {
+                    artists: item.artists,
+                    type: item.type,
+                    track_number: item.track_number,
+                    popularity: item.popularity,
+                    name: item.name,
+                    is_playable: item.is_playable,
+                    id: item.id,
+                    duration_ms: item.duration_ms,
+                    external_urls: item.external_urls,
+                    href: item.href,
+                    uri: item.uri,
+                }
+            })
+        }
+
+        return [];
     } catch (error) {
         throw error;
     }
